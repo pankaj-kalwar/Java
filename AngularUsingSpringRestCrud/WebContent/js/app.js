5e8a6080b6eff1974ab32b64bc6bf6c8259fbf79
kpankaj-cg <pankaj.kalwar@capgemini.com>
2015-12-11 20:45:06 +0530

    Exaplained some more examples

--- AngularUsingSpringRestCrud/WebContent/js/app.js
+++ AngularUsingSpringRestCrud/WebContent/js/app.js
@@ -3,12 +3,58 @@
 	// Main Module Creation
 	angular.module("DemoApp", []);
 	
-	angular.module("DemoApp").controller("EmployeeCtrl", ['$scope', EmployeeCtrl]);
+	/*angular.module("DemoApp").controller("EmployeeCtrl", ['$scope', EmployeeCtrl]);
 	
 	function EmployeeCtrl($scope){
 		$scope.name = "Pankaj";
 		console.log("myname == "+$scope.name);
-	}
+	}*/
+	
+	angular.module("DemoApp").controller("EmployeeCtrl", ['$scope', function EmployeeCtrl($scope){
+		$scope.name = "Pankaj";
+		console.log("myname == "+$scope.name);
+	}]);
+	
+	angular.module("DemoApp").controller("EmployeeCtrl1", ['$scope', 'EmployeeService', function EmployeeCtrl($scope, EmployeeService){
+		$scope.name1 = "Pankaj1";
+		$scope.dataFromService = EmployeeService();
+		console.log("myname == "+$scope.name);
+	}]);
+	
+	
+	// Services
+	angular.module("DemoApp").factory("EmployeeService", function(){
+		//var name = "abc";
+		return function(){
+			return "Value From Service";
+		};
+		
+		/*var function1 = function(){
+			return name;
+		};
+		return{
+			function1 : function1
+		};*/
+	});
+
+	
+	
+	// Template & Template URL --
+	
+	/*template : "<div></div>"
+
+	templateUrl : "some.html"*/
+	
 	
 	
 }());
+
+/*var myFucntion = function(){
+	
+};*/
+
+
+/*(function(){
+	
+}());
+*/
